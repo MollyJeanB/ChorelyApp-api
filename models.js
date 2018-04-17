94dd60dcda54c81e3d53626779a61d4f07c1275d
Molly Jean Bennett <mollyjeanbennett@gmail.com>
2018-04-17 13:38:04 -0700

    write data models

---
+++ models.js
@@ -0,0 +1,45 @@
+"use strict";
+
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+
+const choreSchema = mongoose.Schema({
+  choreName: {type: String, required: true},
+  pointValue: {type: Number, required: true}
+  timesPerWeek: {type: Number, required: true}
+})
+
+const memberSchema = mongoose.Schema({
+  name: {type: String, required: true},
+  color: {type: String, required: true}
+  weekPoints: {type: Number, required: true}
+  totalPoints: {type: Number, required: true}
+})
+
+choreSchema.methods.serialize = function() {
+  return {
+    id: this._id,
+    choreName: this.choreName,
+    pointValue: this.pointValue,
+    timesPerWeek: this.timesPerWeek
+  }
+}
+
+
+memberSchema.methods.serialize = function() {
+ return {
+   id: this._id,
+   name: this.name,
+   color: this.color,
+   weekPoints: this.hours,
+   totalPoints: this.totalPoints
+
+ }
+}
+
+const Chore = mongoose.model("Chore", choreSchema);
+const Member = mongoose.model("Member", memberSchema);
+
+
+
+module.exports = {Chore, Member}
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 const PORT = process.env.PORT || 3000;
 
-const cors = require('cors');
 const {CLIENT_ORIGIN} = require('./config');
 
 app.use(
