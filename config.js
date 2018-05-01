035ceecff7adac686de1f452b9f5bac80f75927b
Molly Jean Bennett <mollyjeanbennett@gmail.com>
2018-05-01 13:20:35 -0700

    quick fix

--- config.js
+++ config.js
@@ -7,5 +7,4 @@ exports.TEST_DATABASE_URL =
 exports.PORT = process.env.PORT || 8080;
 exports.CLIENT_ORIGIN = "http://localhost:3000"
 exports.JWT_SECRET = process.env.JWT_SECRET;
-console.log(process.env.JWT_SECRET)
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || "7d";
